fd3092ace6a0f1e77ca7c77c58e8f770507f4a1e
avi_singh <avi.v.singh@gmail.com>
2023-09-05 18:44:14 +0100

    third commit, create listing page added

--- auctions/views.py
+++ auctions/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from .models import *
 
 from .models import User
 
@@ -61,3 +62,10 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    if request.method == "GET":
+        return render(request, "auctions/create_listing.html", {
+            "categories": Category.objects.all()
+        })
